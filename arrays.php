65a83e18bf9ffa866571822ce82302718a478652
Yuri Lobzaev <44752333+lobzaev@users.noreply.github.com>
2022-08-25 10:46:29 +0200

    Init
    Array tasks

---
+++ arrays.php
@@ -0,0 +1,24 @@
+
+# 1 -------------- Array-Füllung---------------------
+
+# 1.1 - manual array filling without keys
+# items: Apfel,Banane,Gurke,Kartoffel
+
+$array1 = [];
+
+print_r($array1);
+
+/*
+	result: Array ( [0] => Apfel [1] => Banane [2] => Gurke [3] => Kartoffel )
+*/
+
+# 1.2 - manual array filling with keys
+# items: name:Alex, age:32, city:Hamburg
+
+$array2 = [];
+
+print_r($array2);
+
+/*
+	result: Array ( [name] => Alex [age] => 32 [city] => Hamburg )
+*/
